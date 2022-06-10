a5a2cd219a1b42d5b4c30f50167707e66eb227c4
Youssef Sherif <joeshiref@hotmail.com>
2022-06-10 04:38:46 +0200

    Time: 0 ms (100.00%), Space: 5.7 MB (87.19%) - LeetHub

--- 367-valid-perfect-square/367-valid-perfect-square.cpp
+++ 367-valid-perfect-square/367-valid-perfect-square.cpp
@@ -1,8 +1,17 @@
 class Solution {
 public:
     bool isPerfectSquare(int num) {
-        double sq=sqrt(num);
-        int sq1=sqrt(num);
-        return sq==sq1;
+        long long s=0,e=num,mid;
+        while(s<=e)
+        {
+            mid=s+(e-s)/2;
+            if(mid*mid==num)
+                return true;
+            else if(mid*mid>num)
+                e=mid-1;
+            else
+                s=mid+1;
+        }
+        return false;
     }
 };
