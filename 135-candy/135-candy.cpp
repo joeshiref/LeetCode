3c232817ef4c20addfc4f5f965fedc999ceaf3dc
Youssef Sherif <joeshiref@hotmail.com>
2022-07-04 22:35:16 +0200

    Time: 46 ms (22.34%), Space: 17.9 MB (41.00%) - LeetHub

---
+++ 135-candy/135-candy.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+        vector<int>candies(n,1);
+        for(int i=1;i<n;i++)
+        {
+            if(ratings[i]>ratings[i-1])
+                candies[i]=candies[i-1]+1;
+        }
+        for(int i=n-2;i>=0;i--)
+        {
+            if(ratings[i]>ratings[i+1])
+                candies[i]=max(candies[i+1]+1,candies[i]);
+        }
+        int sum=0;
+        for(auto num:candies)
+            sum+=num;
+        return sum;
+    }
+};
