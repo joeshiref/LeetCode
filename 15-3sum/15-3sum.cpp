c1dd13786fae67ff043916de9322a487191fa638
Youssef Sherif <joeshiref@hotmail.com>
2022-08-21 14:47:27 +0200

    Time: 81 ms (94.07%), Space: 20 MB (71.54%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -1,37 +1,34 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-	std::vector<vector<int>> result;
-	if (nums.empty()) {
-		return result;
-	}
-
-	std::size_t n_size = nums.size();
-	std::sort(nums.begin(), nums.end());
-	for (int i = 0; i < n_size; ++i) {
-		// all numbers from now on will be greater than 0, no point in continuing
-		if (nums[i] > 0) break;
-
-		// we have seen this number & combo before; skip
-		if (i > 0 and nums[i] == nums[i-1]) continue;
-
-		int left = i+1, right = n_size - 1;
-		while (left < right) {
-			int sum = nums[i] + nums[left] + nums[right];
-			if (sum < 0) {
-				++left;
-			} else if (sum > 0) {
-				--right;
-			} else {
-				result.push_back({nums[i], nums[left], nums[right]});
-				int last_left = nums[left], last_right = nums[right];
-				// we have seen this number & combo before; skip
-				while (left < right && nums[left] == last_left) ++left;
-				while (left < right && nums[right] == last_right) --right;
-			}
-		}
-
-	}
-	return result;
-}
+        vector<vector<int>>ans;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]>0)
+                break;
+            if (i > 0 and nums[i] == nums[i-1])
+                continue;
+            int left=i+1,right=nums.size()-1;
+            while(left<right)
+            {
+                int sum = nums[i] + nums[left] + nums[right];
+                if(sum < 0)
+                    left++;
+                else if(sum > 0)
+                    right--;
+                else
+                {
+                    ans.push_back({nums[i] , nums[left] , nums[right]});
+                    int lastLeft = nums[left];
+                    int lastRight = nums[right];
+                    while(left<right && nums[left]==lastLeft)
+                        left++;
+                    while(left<right && nums[right]==lastRight)
+                        right--;
+                }
+            }
+        }
+        return ans;
+    }
 };
