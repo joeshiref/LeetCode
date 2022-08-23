24cd74a3ba15bceaeaa3572207562a1d085ca725
Youssef Sherif <joeshiref@hotmail.com>
2022-08-23 10:40:53 +0200

    Time: 86 ms (22.10%), Space: 20.5 MB (65.49%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.cpp
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -1,13 +1,27 @@
 class Solution {
 public:
+    string sortString(string str)
+    {
+        int freq[26]={};
+        for(auto c:str)
+        {
+            freq[c-'a']++;
+        }
+        string ret="";
+        for(int i=0;i<26;i++)
+        {
+            for(int j=0;j<freq[i];j++)
+                ret+=(i+'a');
+        }
+        return ret;
+    }
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         unordered_map<string,vector<string>> mp;
         for(auto s:strs)
         {
             string tmp;
             tmp = s;
-            sort(tmp.begin(),tmp.end());
-            mp[tmp].push_back(s);
+            mp[sortString(tmp)].push_back(s);
         }
         vector<vector<string>> ret;
         for(auto m:mp)
