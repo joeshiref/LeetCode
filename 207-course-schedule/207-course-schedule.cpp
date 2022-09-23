5db6210c1e893f5da422ed641ed6dcd10d474d2c
Youssef Sherif <joeshiref@hotmail.com>
2022-09-23 18:50:43 +0200

    Time: 59 ms (13.34%), Space: 14.2 MB (42.86%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,43 +1,43 @@
 class Solution {
 public:
     
-    int timer;
-    int vis[2001];
-    vector<vector<int> >adj;
-    bool dfs(int source)
+    vector<vector<int>>adj;
+    vector<int>vis,inrec;
+    bool hasCycle;
+    void dfs(int source)
     {
-        if(vis[source]==1)
-            return true;
-        if(vis[source]==0)
+        vis[source]=inrec[source]=1;
+        for(int i=0;i<adj[source].size();i++)
         {
-            vis[source]=1;
-            for(int i=0;i<adj[source].size();i++)
-            {
-                int child = adj[source][i];
-                if(dfs(child))
-                    return true;
-            }
+            int child = adj[source][i];
+            if(inrec[child])
+                hasCycle = 1;
+            else if(!vis[child])
+                dfs(child);
         }
-        vis[source]=2;
-        return false;
+        inrec[source] = 0;
     }
     
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-
         adj.resize(numCourses);
-        for(auto pre:prerequisites)
+        for(auto vec:prerequisites)
         {
-            int a=pre[0];
-            int b=pre[1];
+            int a=vec[0];
+            int b=vec[1];
             adj[b].push_back(a);
         }
+        vis = vector<int>(numCourses,0);
+        inrec = vector<int>(numCourses,0);
+        
+        
         for(int i=0;i<numCourses;i++)
         {
-            if(dfs(i))
+            hasCycle=0;
+            dfs(i);
+            if(hasCycle)
                 return false;
         }
         return true;
-        
         
     }
 };
