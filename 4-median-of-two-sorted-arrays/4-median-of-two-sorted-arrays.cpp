69d7985f2c80a89f69ddd8d96a481e96528afe9f
Youssef Sherif <joeshiref@hotmail.com>
2022-09-23 15:24:36 +0200

    Time: 78 ms (30.35%), Space: 89.4 MB (64.53%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
@@ -1,25 +1,42 @@
 class Solution {
 public:
-    
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-    int N1 = nums1.size();
-    int N2 = nums2.size();
-    if (N1 < N2) return findMedianSortedArrays(nums2, nums1);	// Make sure A2 is the shorter one.
-    
-    int lo = 0, hi = N2 * 2;
-    while (lo <= hi) {
-        int mid2 = (lo + hi) / 2;   // Try Cut 2 
-        int mid1 = N1 + N2 - mid2;  // Calculate Cut 1 accordingly
         
-        double L1 = (mid1 == 0) ? INT_MIN : nums1[(mid1-1)/2];	// Get L1, R1, L2, R2 respectively
-        double L2 = (mid2 == 0) ? INT_MIN : nums2[(mid2-1)/2];
-        double R1 = (mid1 == N1 * 2) ? INT_MAX : nums1[(mid1)/2];
-        double R2 = (mid2 == N2 * 2) ? INT_MAX : nums2[(mid2)/2];
+        if(nums1.size()>nums2.size())
+            return findMedianSortedArrays(nums2,nums1);
         
-        if (L1 > R2) lo = mid2 + 1;		// A1's lower half is too big; need to move C1 left (C2 right)
-        else if (L2 > R1) hi = mid2 - 1;	// A2's lower half too big; need to move C2 left.
-        else return (max(L1,L2) + min(R1, R2)) / 2;	// Otherwise, that's the right cut.
+        int x = nums1.size();
+        int y = nums2.size();
+        int len = x+y;
+        int half = len/2;
+        int s = 0,e = x;
+        while(s<=e)
+        {
+            int mid1 = (s+e)/2;
+            int mid2 = (x+y+1)/2 - mid1;
+            
+            
+            int leftA = (mid1==0)?INT_MIN:nums1[mid1-1];
+            int rightA = (mid1==x)?INT_MAX:nums1[mid1];
+            
+            
+            int leftB = (mid2==0)?INT_MIN:nums2[mid2-1];
+            int rightB = (mid2==y)?INT_MAX:nums2[mid2];
+            
+            
+            if(leftA <= rightB && leftB <= rightA)
+            {
+                if(len%2==0)
+                    return((double)max(leftA,leftB)+min(rightA,rightB))/2.0;
+                else
+                    return ((double)max(leftA,leftB));
+            }
+            else if(leftA>rightB)
+                e=mid1-1;
+            else
+                s=mid1+1;
+        }
+        
+        return 0.0;
     }
-    return -1;
-    } 
 };
