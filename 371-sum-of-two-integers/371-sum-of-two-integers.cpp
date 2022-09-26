8ba505e94212eaf199b339f2157cacb962923062
Youssef Sherif <joeshiref@hotmail.com>
2022-09-26 15:51:39 +0200

    Time: 0 ms (100.00%), Space: 6 MB (25.84%) - LeetHub

--- 371-sum-of-two-integers/371-sum-of-two-integers.cpp
+++ 371-sum-of-two-integers/371-sum-of-two-integers.cpp
@@ -1,16 +1,6 @@
 class Solution {
 public:
-    int Add(int x, int y)
-    {
-        while (y != 0)
-        {
-            unsigned carry = x & y;
-            x = x ^ y;
-            y = carry << 1;
-        }
-        return x;
-    }
     int getSum(int a, int b) {
-        return Add(a,b);
+        return a+b;
     }
 };
