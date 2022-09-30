49b2ed2462e69ada77da2f0c87e4d88d6d0d82dd
Youssef Sherif <joeshiref@hotmail.com>
2022-09-30 17:30:00 +0200

    Time: 539 ms (70.22%), Space: 164.9 MB (87.12%) - LeetHub

--- 146-lru-cache/146-lru-cache.cpp
+++ 146-lru-cache/146-lru-cache.cpp
@@ -1,35 +1,40 @@
-class LRUCache
-{
-    public:
-        list<pair<int,int>> l;
-        unordered_map<int,list<pair<int, int>>::iterator> m;
-        int size;
-        LRUCache(int capacity)
+class LRUCache {
+public:
+    list<pair<int,int> >lst;
+    unordered_map<int, list<pair<int,int> >::iterator > mp;
+    int size;
+    LRUCache(int capacity) {
+        size = capacity;
+    }
+    
+    int get(int key) {
+        if(mp.find(key)==mp.end())
+            return -1;
+        lst.splice(lst.begin(),lst,mp[key]);
+        return mp[key]->second;
+    }
+    
+    void put(int key, int value) {
+        if(mp.find(key)!=mp.end())
         {
-            size=capacity;
+            lst.splice(lst.begin(),lst,mp[key]);
+            mp[key]->second=value;
+            return;
         }
-        int get(int key)
+        if(size == lst.size())
         {
-            if(m.find(key)==m.end())
-                return -1;
-            l.splice(l.begin(),l,m[key]);
-            return m[key]->second;
+            auto deletedKey = lst.back().first;
+            lst.pop_back();
+            mp.erase(deletedKey);
         }
-        void put(int key, int value)
-        {
-            if(m.find(key)!=m.end())
-            {
-                l.splice(l.begin(),l,m[key]);
-                m[key]->second=value;
-                return;
-            }
-            if(l.size()==size)
-            {
-                auto d_key=l.back().first;
-                l.pop_back();
-                m.erase(d_key);
-            }
-            l.push_front({key,value});
-            m[key]=l.begin();
-        }
+        lst.push_front({key,value});
+        mp[key]=lst.begin();
+    }
 };
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
