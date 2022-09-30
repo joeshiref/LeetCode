1295ff35c0088515e89493a49e6807b07bc0ad3a
Youssef Sherif <joeshiref@hotmail.com>
2022-09-30 17:44:28 +0200

    Time: 70 ms (49.61%), Space: 19.1 MB (69.26%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -3,23 +3,23 @@ public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         sort(intervals.begin(),intervals.end());
         vector<vector<int>> ans;
-        int start = intervals[0][0];
-        int end = intervals[0][1];
+        int s = intervals[0][0];
+        int e = intervals[0][1];
         for(int i=1;i<intervals.size();i++)
         {
-            int s,e;
-            s=intervals[i][0];
-            e=intervals[i][1];
-            if(end>=s)
-                end = max(end,e);
+            int curStart = intervals[i][0];
+            int curEnd = intervals[i][1];
+            if(e>=curStart)
+                e=max(curEnd,e);
             else
             {
-                ans.push_back({start,end});
-                start = s;
-                end = e;
+                ans.push_back({s,e});
+                s=curStart;
+                e=curEnd;
+                //ans.push_back({curStart,curEnd});
             }
         }
-        ans.push_back({start,end});
+        ans.push_back({s,e});
         return ans;
     }
 };
