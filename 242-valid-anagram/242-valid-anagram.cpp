a1a3aeac7fb7ecd729be0bfa1dcf353995ac88b8
Youssef Sherif <joeshiref@hotmail.com>
2022-10-04 15:49:35 +0200

    Time: 19 ms (48.06%), Space: 7.4 MB (55.92%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,8 +1,17 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        return s==t;
+        int freq[26]={};
+        int freq2[26]={};
+        for(auto &c:s)
+            freq[c-'a']++;
+        for(auto &c:t)
+            freq2[c-'a']++;
+        for(int i=0;i<26;i++)
+        {
+            if(freq[i]!=freq2[i])
+                return false;
+        }
+        return true;
     }
 };
