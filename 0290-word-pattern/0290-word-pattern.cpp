e048223e91563729e9846319ba795682584303b3
Youssef Sherif <joeshiref@hotmail.com>
2023-01-02 00:36:23 +0200

    Time: 0 ms (100.00%), Space: 6.6 MB (19.95%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) 
+    {
+        map<char, string>chToWordMap;
+        map<string, char>wordToChMap;
+        s.push_back(' ');
+        string currWord = "";
+        int chIdx = 0;
+        for (int  i = 0; i < s.size(); i++)
+        {
+            if(s[i] == ' ') 
+            {
+                char currCh = pattern[chIdx++];
+                if (chToWordMap.count(currCh))
+                {
+                    string alreadyMappedWord = chToWordMap[currCh];
+                    if (alreadyMappedWord != currWord) 
+                        return false; 
+                }
+                else if (wordToChMap.count(currWord))
+                {
+                    char alreadyMappedCh = wordToChMap[currWord];
+                    if (alreadyMappedCh != currCh) 
+                        return false; 
+                }
+                else
+                {
+                    chToWordMap[currCh] = currWord;
+                    wordToChMap[currWord] = currCh;
+                }
+                currWord = "";
+            }
+            else currWord.push_back(s[i]);
+        }
+        return (chIdx == pattern.size());
+        
+    }
+};
