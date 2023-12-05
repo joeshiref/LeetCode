dd4bb60aa9f4b47b9e202caae7489776777c347c
Youssef Sherif <joeshiref@hotmail.com>
2023-12-05 19:11:49 +0200

    Time: 0 ms (100.00%), Space: 6.3 MB (73.09%) - LeetHub

--- 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
@@ -1,14 +1,6 @@
 class Solution {
 public:
     int numberOfMatches(int n) {
-        int cnt=0;
-        while(n>=2)
-        {
-            int tmp = n;
-            cnt += n/2;
-            n/=2;
-            n+=(tmp%2);
-        }
-        return cnt;
+        return n-1;
     }
 };
