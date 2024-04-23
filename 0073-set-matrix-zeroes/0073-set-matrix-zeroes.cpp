a64fc50a7acb5899469af27c6cf679254a96e313
Youssef Sherif <joeshiref@hotmail.com>
2024-04-23 02:08:24 +0200

    Time: 11 ms (47.73%), Space: 16.4 MB (24.50%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -16,20 +16,46 @@ public:
         n = matrix.size();
         m = matrix[0].size();
         vector<pair<int,int> >vec;
-        
+        bool fr = 0, fc = 0;
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
+                
                 if(matrix[i][j]==0)
-                    vec.push_back({i,j});
+                {
+                    if(i==0)fr=1;
+                    if(j==0)fc=1;
+                    matrix[0][j]=0;
+                    matrix[i][0]=0;
+                }
             }
         }
-        for(int i=0;i<vec.size();i++)
+        for(int i=1;i<n;i++)
         {
-            pair<int,int> cell = vec[i];
-            setRows(cell.first,matrix);
-            setCols(cell.second,matrix);
+            for(int j=1;j<m;j++)
+            {
+                if(matrix[0][j]==0)
+                {
+                    for(int f=0;f<n;f++)
+                        matrix[f][j]=0;
+                }
+                else if(matrix[i][0]==0)
+                {
+                    for(int f=0;f<m;f++)
+                        matrix[i][f]=0;
+                }
+            }
+        }
+        if(fr)
+        {
+            for(int f=0;f<m;f++)
+                matrix[0][f]=0;
+        }
+        if(fc)
+        {
+            for(int f=0;f<n;f++)
+                matrix[f][0]=0;
         }
         return;
     }
