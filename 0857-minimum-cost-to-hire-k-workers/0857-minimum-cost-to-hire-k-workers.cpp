355737a028de0c916cec77b89ba61d256626ebe0
Youssef Sherif <joeshiref@hotmail.com>
2024-05-11 17:36:06 +0300

    Time: 29 ms (47.77%), Space: 26.5 MB (38.83%) - LeetHub

---
+++ 0857-minimum-cost-to-hire-k-workers/0857-minimum-cost-to-hire-k-workers.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    double mincostToHireWorkers(vector<int>& quality, vector<int>& wage, int k) {
+        int n = quality.size();
+        double totalCost = numeric_limits<double>::max();
+        double currentTotalQuality = 0;
+        vector<pair<double, int>> wageToQualityRatio;
+
+        for (int i = 0; i < n; i++) {
+            wageToQualityRatio.push_back(
+                {static_cast<double>(wage[i]) / quality[i], quality[i]});
+        }
+
+        sort(wageToQualityRatio.begin(), wageToQualityRatio.end());
+
+        priority_queue<int> highestQualityWorkers;
+
+        for (int i = 0; i < n; i++) {
+            highestQualityWorkers.push(wageToQualityRatio[i].second);
+            currentTotalQuality += wageToQualityRatio[i].second;
+
+            
+            if (highestQualityWorkers.size() > k) {
+                currentTotalQuality -= highestQualityWorkers.top();
+                highestQualityWorkers.pop();
+            }
+
+            if (highestQualityWorkers.size() == k) {
+                totalCost = min(totalCost, currentTotalQuality *
+                                               wageToQualityRatio[i].first);
+            }
+        }
+        return totalCost;
+    }
+};
